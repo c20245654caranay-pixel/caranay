119d5997663315a1701ee0e28ae31de6fb069d7e
c2024565caranay-bit <c20245654.caranay@csav.edu.ph>
2026-09-04 15:39:32 +0800

    functions

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("Hello, World!");
+console.log("From script.js file.");
+
+const myName = "Nelsie";
+let age = 18;
+age = 20;
+myNumber = 9945245958;
+myaddress = "Manapla";
+
+
+console.log(`My name is ${myName}, ${age} yrs old.`);
+console.log(`Number: ${myNumber}`);
+console.log(`Address: ${myaddress}`);
+console.log(67);
+
+function greet(greetings, name) {
+    if (Array.isArray(name)) {
+        names = name.join(", ");
+    }
+    return `${greetings} sa emu, ${name}`;
+
+    console.log(greet("Mayong aga", ["Nelsie", "Bai", "Soy", "Tol", "Erp"]));
+}
+
