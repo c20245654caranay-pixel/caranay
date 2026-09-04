2f395ed5636a36805c8e947e9be44745567f4e7d
c20245654caranay-bit <c20245654.caranay@csav.edu.ph>
2026-09-04 17:48:15 +0800

    addEventListener

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 console.log("Hello, World!");
 console.log("From script.js file.");
 
-const myName = "Nelsie";
+const myName = "Arabelle";
 let age = 18;
 age = 20;
-myNumber = 9945245958;
+myNumber = 9638409151;
 myaddress = "Manapla";
 
 
@@ -13,22 +13,46 @@ console.log(`Number: ${myNumber}`);
 console.log(`Address: ${myaddress}`);
 console.log(67);
 
-function greet(greetings, name) {
-    if (Array.isArray(name)) {
-        names = name.join(", ");
+function greet(greetings, names = []) {
+    if (Array.isArray(names)) {
+        names = names.join(',');
     }
-    return `${greetings} sa emu, ${name}`;
-
-    console.log(greet("Mayong aga", ["Nelsie", "Bai", "Soy", "Tol", "Erp"]));
+    return `${greetings} sa enyu, ${names}`;
 }
+console.log(greet("Mayong aga", ["Arabelle", "Bai", "Soy", "Erp"]));
+
 const heading = document.querySelector("h1");
-heading.textContent = "Antonio";
-heading.style.color = "blue";
+heading.textContent = "Ronel";
+heading.style.color = "purple";
 heading.style.backgroundColor = "yellow";
+heading.style.fontSize = "50px";
 console.log(heading.textContent);
 
-const contactHeader = document.querySelector("#contact h2");
-console.log(contactsHeading);
+heading.addEventListener("click", function () {
+    heading.style.color = "red";
+    console.log("Heading was clicked!");
+});
+
+const contactHeading = document.querySelector("#contact h2");
+console.log(contactHeading)
 
 const projectsHeading = document.querySelector("#services h2");
-console.log(projectsHeading);
+console.log(projectsHeading)
+
+const toggleButton = document.querySelector("#switch");
+const body = document.querySelector("body");
+let isOff = false;
+
+toggleButton.addEventListener("click", function () {
+    isOff = !isOff;
+
+    if (isOff) {
+        body.style.backgroundColor = "black";
+        body.style.color = "white";
+    } else {
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+    }
+});
+
+
